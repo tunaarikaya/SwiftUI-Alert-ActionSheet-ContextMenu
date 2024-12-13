cdcb7f815015e17a13524a92b892ebb6eab3c0fe
Mehmet Tuna Arıkaya <m.tunaarikaya@gmail.com>
2024-12-13 21:24:58 +0300

    main
    main

---
+++ kullaniciEtkilesimi/kullaniciEtkilesimi/ContentView.swift
@@ -0,0 +1,58 @@
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var alert = false
+    @State private var actionSheet = false
+    
+    var body: some View {
+        VStack(spacing:100) {
+            Button("Alert"){
+                alert = true
+            }.alert("Başlık",isPresented: $alert,actions: {
+                
+                Button("İptal",role: .cancel,action: {
+                    print("İptal seçildi")
+                })
+                Button("Tamam",role: .destructive,action: {
+                    print("Tamam seçildi")
+                })
+                
+            },message: {Text("İçerik")})
+            
+            Button("Action Sheet"){
+                actionSheet = true
+            }.actionSheet(isPresented: $actionSheet){
+                ActionSheet(title: Text("Başlık"),
+                            message: Text("İçerik"),
+                            buttons: [
+                                .default(Text("İptal"),action: {
+                                    print("İptal seçildi")
+                                }),
+                                .destructive(Text("Tamam"),action: {
+                                    print("Tamam seçildi")
+                                })
+                            ])
+            }
+            
+            Text("Context Menu")
+                .contextMenu{
+                    Button{
+                        print("Resim seçildi")
+                    }label: {
+                        Label("Resim Çek",systemImage: "camera")
+                    }
+                    
+                    Button{
+                        print("Video seçildi")
+                    }label: {
+                        Label("Video Kaydet",systemImage: "video")
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kullaniciEtkilesimi/kullaniciEtkilesimi/kullaniciEtkilesimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kullaniciEtkilesimiApp.swift
+//  kullaniciEtkilesimi
+//
+//  Created by Mehmet Tuna Arıkaya on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct kullaniciEtkilesimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
